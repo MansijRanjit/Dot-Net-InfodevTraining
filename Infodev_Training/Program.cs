add8835d85318a72faaa276e73866e96ed9aef52
Mansij Ranjit <mansijranjit@gmail.com>
2022-09-29 10:47:20 +0545

    Day2

--- Infodev_Training/Program.cs
+++ Infodev_Training/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleApp
     public class Car
     {
         int Speed;
-        public int HorsePower { get; set; }
+        int _horsepower;
+        public int HorsePower
+        {
+            get
+            {
+                return _horsepower; 
+            }
+            set
+            {
+                _horsepower = value;
+            }
+        }
         public int Wheels { get; set; }
         public string Type { get; set; }
         public string Brand { get; set; }
@@ -19,13 +30,59 @@ namespace ConsoleApp
         Employee=50,
         Owner
     }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return ($"{FirstName} {MiddleName} {LastName}");
+            }
+        }
+    }
+    public class Student : Person
+    {
+        public string Faculty { get; set; }
+        public DateTime JoinDate { get; set; }
+
+        //public Student(string Faculty) {   }
+
+        public int Age()
+        {
+            return 25;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             //ClassEg();
             //Datatype();
-             EnumEg();
+            // EnumEg();
+            Interpolation();
+        }
+        public static void Interpolation()
+        {
+            int a = 50;
+            int b = 100;
+            //string formatting
+            Console.WriteLine("This is a string {1} and string {0}", a, b);
+            string str= String.Format("This is a string {1} and string {0}", a, b);
+            Console.WriteLine(str);
+            
+
+            //string interpolation:
+            Console.WriteLine($"This is a string {b} and string {a}");
+
+
+            Person s1 = new Person();
+            s1.FirstName = "Mansij";
+            s1.LastName = "Ranjit";
+            Console.WriteLine(s1.FullName);
+
         }
         public static void ClassEg()
         {
